3b0018868a439082d5731fc22b31a4fb5967579e
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-03-07 17:59:35 -0800

    Wired up new landing page

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'example_project.views.home', name='home'),
+    url(r'^$', 'example_project.views.home', name='home'),
     # url(r'^example_project/', include('example_project.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
